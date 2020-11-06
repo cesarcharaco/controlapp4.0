8b46fca7c4ed8a598318aec94c0e90da33fbc80c
Jhon-Dante <imagine.breacker@gmail.com>
2020-11-05 23:08:07 -0400

    Arreglo checks - referencia

--- resources/views/flow/orden.blade.php
+++ resources/views/flow/orden.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{--@extends('layouts.app')
 
 @section('content')
 
@@ -132,9 +132,19 @@
     	</div>
     </div>
 
-@endsection
+@endsection--}}
 
 
 
 
 
+Confirme su orden antes de proceder al pago via Flow<br /><br />
+Orden N°: {{$orden['orden_compra']}}<br />
+Monto: {{$orden['monto']}}<br />
+Descripción: {{$orden['concepto']}}<br />
+Email Pagador (Opcional): {{$orden['email_pagador']}}<br />
+<form method="post" action="{{config('flow.url_pago')}}">
+{{ csrf_field() }}
+<input type="hidden" name="parameters" value="{{$orden['flow_pack'] }}" />
+<button type="submit">Pagar en Flow</button>
+</form>
