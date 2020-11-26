cf7c35b7f4118a03a8e17195b93120375528524f
Jhon-Dante <imagine.breacker@gmail.com>
2020-11-26 14:58:33 -0400

    Imagenes actualizadas - controlNice

--- database/seeds/MembresiasTableSeeder.php
+++ database/seeds/MembresiasTableSeeder.php
@@ -12,21 +12,21 @@ class MembresiasTableSeeder extends Seeder
     public function run()
     {
         \DB::table('membresias')->insert([
-            'url_imagen' => 'assets/images/membresia_base.png',
+            'url_imagen' => 'assets/images/membresia_base.jpg',
         	'nombre' => 'Base',
         	'cant_inmuebles' => '50',
         	'monto' => '0'
         ]);
 
         \DB::table('membresias')->insert([
-            'url_imagen' => 'assets/images/membresia_media.png',
+            'url_imagen' => 'assets/images/membresia_media.jpg',
         	'nombre' => 'Media',
         	'cant_inmuebles' => '100',
         	'monto' => '15'
         ]);
 
         \DB::table('membresias')->insert([
-            'url_imagen' => 'assets/images/membresia_plus.png',
+            'url_imagen' => 'assets/images/membresia_plus.jpg',
         	'nombre' => 'Plus',
         	'cant_inmuebles' => '200',
         	'monto' => '25'
